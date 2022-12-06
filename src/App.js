6fa4ba80d9e6673e058f6ceee76f4b06fa1a8662
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-12-06 15:15:05 +0100

    table

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import GlobalChart from './components/GlobalChart';
 import HeaderInfos from './components/HeaderInfos';
+import Table from './components/Table';
 
 const App = () => {
 	const [coinsData, setCoinsData] = useState([]);
@@ -20,6 +21,7 @@ const App = () => {
 				<HeaderInfos />
 				<GlobalChart coinsData={coinsData} />
 			</header>
+			<Table coinsData={coinsData} />
 		</div>
 	);
 };
---
+++ src/components/TableLine.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TableLine = ({ coin }) => {
+	return (
+		<div>
+			<h2>{coin.symbol}</h2>
+		</div>
+	);
+};
+
+export default TableLine;
